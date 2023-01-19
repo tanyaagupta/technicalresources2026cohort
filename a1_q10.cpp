cd5f9d82144972b3ae7c6a446c7edf17e05cfb78
tanyaagupta <120319976+tanyaagupta@users.noreply.github.com>
2023-01-20 00:09:58 +0530

    question10 of assignment1

---
+++ a1_q10.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int number,i=1,j,count;
+	cout<<"Enter Number:";
+	cin >> number;
+	while (i <= number){
+   		count = 0;
+    	if(number % i == 0){
+      		j = 1;
+      		while(j <= i)	{
+      			if(i % j == 0)
+      			{ count++;
+				}				j++;}
+			if(count == 2){
+				cout << i << " is a Prime Factor\n";
+			} 
+      	}    	i++;
+   	}
+ 	return 0;
+}
