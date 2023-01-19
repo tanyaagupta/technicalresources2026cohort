80d441a17260a4eaafc7278613f200a0af43e285
tanyaagupta <120319976+tanyaagupta@users.noreply.github.com>
2023-01-19 23:41:07 +0530

    question4 of assignment1

---
+++ a1_q4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int r1,r2;
+    cout<<"enter lower limit:";
+    cin>>r1;
+    cout<<"enter upper limit:";
+    cin>>r2;
+    int i;
+    for (i=r1;i<=r2;i++){
+        cout<<" "<<i;
+    }
+}
